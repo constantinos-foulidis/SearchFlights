6d512c92b9384d4a50cb63bf5fcdbbefbf2d5d15
constantinos <constantine_flds@hotmail.com>
2019-06-30 00:38:45 +0300

    config dependasies

---
+++ src/app/airport-names/airport-names.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AirportNames } from '../airportName';
+import { AIRPORTNAMES } from '../mock-airport';
+
+
+@Component({
+  selector: 'app-airport-names',
+  templateUrl: './airport-names.component.html',
+  styleUrls: ['./airport-names.component.scss']
+})
+export class AirportNamesComponent implements OnInit {
+
+      AirportNames = AIRPORTNAMES;
+selectedAirport: AirportNames;
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+  onSelect(airportname: AirportNames): void {
+      this.selectedAirport = airportname;
+      console.log("im here"+airportname);
+    }
+}
